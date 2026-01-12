efd2ce0af57e7a80fd98d183febc15e337b280a3
ariwh659 <ariwh659@gmail.com>
2026-01-12 16:41:32 +0800

    Add files via upload

---
+++ 2026_01_11_125645_create_transactions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->integer('qty');
+            $table->integer('total_price');
+            $table->timestamps();
+
+            $table->foreign('product_id')
+                  ->references('id')
+                  ->on('products')
+                  ->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
